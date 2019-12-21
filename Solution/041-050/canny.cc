64102c1bfd2b931947487f12ca31725077c8cf78
Dream9 <45119286+Dream9@users.noreply.github.com>
2019-12-21 15:24:11 +0800

    Add files via upload

---
+++ Solution/041-050/canny.cc
@@ -0,0 +1,30 @@
+#include"Solution/041-050/canny.h"
+#include"Solution/base.h"
+
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+
+namespace digital {
+
+void Canny::operator()() {
+	cv::Mat data = cv::imread(getPath(), cv::IMREAD_GRAYSCALE);
+	if (data.empty()) {
+		dealException(kFileError);
+		return;
+	}
+	cv::Mat img;
+
+#ifdef USE_OPENCVLIB
+	cv::Canny(data, img, threshold_1_, threshold_2_, aperture_size_, use_L2gradient_);
+#else
+	detail::Canny(data, img, threshold_1_, threshold_2_, aperture_size_, use_L2gradient_);
+#endif
+
+	if (needShowOriginal())
+		show(&data, &img);
+	else
+		show(&img);
+
+}
+
+}//!namespace digital
