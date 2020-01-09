3dfc735bdf0c1a83886134c30144a72ed04a4865
Dream9 <45119286+Dream9@users.noreply.github.com>
2020-01-09 19:54:35 +0800

    Add files via upload

--- Solution/061-070/connect_number.cc
+++ Solution/061-070/connect_number.cc
@@ -19,33 +19,41 @@ void ConnectNumber::operator()() {
 	}
 	cv::Mat img;
 
+	coutInfo("关于边界的处理这里是当成0看待");
 	detail::setConnectNumber(data, img, line_type_);
 
 	//cv::equalizeHist(img, img);
 
 	
-	cv::Scalar k[] = { {255,0,0},{0,255,0},{0,0,255},
+	cv::Scalar k[] = { {0,0, 255},{0,255,0},{255, 0, 0,},
+						{255,255,0},{255,0,255},
 						{128,128,128},{70,70,70},{200,200,200},
 						{128,128,0},{0,128,128},{128,0,128} };
 
 	cv::Mat img2 = cv::Mat::zeros(data.size(), CV_8UC3);
 	auto iter = img2.data;
 	size_t jump = img2.elemSize();
-	auto set_color = [&k,&iter,jump](uint8_t* cur) {
-		if (*cur == 0) {
+	auto dataiter = data.data;
+
+	coutInfo("0连通数和背景被刻意区分开来");
+	//为不同的连通数赋予不同的颜色，注意，对于连通数为0的点其和背景无异，这也体现了连通数的图像意义
+	auto set_color = [&k,&iter, &dataiter,jump](uint8_t* cur) {
+		if (*dataiter== 0) {
 			iter += jump;
+			++dataiter;
 			return;
 		}
-		auto count = *cur - 1;
-		//coutInfo(std::to_string(count).c_str());
+		auto count = *cur;
 
 		iter[0] = uint8_t(k[count][0]);
 		iter[1] = uint8_t(k[count][1]);
 		iter[2] = uint8_t(k[count][2]);
 
 		iter += jump;
+		++dataiter;
 	};
 	detail::grayscaleTransform(img, set_color);
+	assert(dataiter == data.data + data.total());
 
 	if (needShowOriginal())
 		show(&data, &img2);
